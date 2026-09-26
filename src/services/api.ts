feb85655dee3a0d6e900dcc77e9ccc33c0acd710
Meghan Sharma <officialmeghan1@gmail.com>
2026-09-26 15:28:20 +0530

    minor change

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL_DEV || '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
 });
 
 // Request interceptor to attach token
